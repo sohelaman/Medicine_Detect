6725355996f67db0dbd977f5eda3c30df3e391ba
Sohel Aman Khan <sohelamankhan@gmail.com>
2016-01-18 00:14:42 +0600

    Experimental changes and added opencv-python script.

--- med_finder.m
+++ med_finder.m
@@ -2,23 +2,29 @@
 clear all; clc;
 %imaqhwinfo;
 %webcamlist;
+IMG = imread('sample.jpg');
+IM = imresize(IMG, [450,800]);
+IM = imrotate(IMG, 90);
+%IM = snap();
 
-%IM = imrotate(imread('sample.jpg'), 90);
-IM = snap();
 
 %parameters
-sensitivity = 0.94;
+sensitivity = 0.95;
 objectPolarity = 'bright';
 rad = [50, 60];
 emptyStrip = false;
 
+
+
+
 while(true)
     [c,r] = imfindcircles(IM,rad,'ObjectPolarity',objectPolarity,'Sensitivity',sensitivity);
     if(size(c) < 1 )
-        if rad(1) > 0
+        if rad(1) > 10
             rad = rad - 10;
         else
             emptyStrip = true;
+            break;
         end
     else
         break;
@@ -27,29 +33,33 @@ end
 imshow(IM); h = viscircles(c,r);
 
 %sorting
-d = sortrows(c, 2);
+if( size(c) > 0 )
+    d = sortrows(c, 2);
 
-%centers
-hold on;
-for n = 1:size(d)
-    plot(d(n,1),d(n,2),'r.','MarkerSize',20);
+    %centers
+    hold on;
+    for n = 1:1%size(d)
+        plot(d(n,1),d(n,2),'r.','MarkerSize',20);
+    end
+
+    disp(d);
+    if(~emptyStrip)
+        nextAvailTablet = d(1,:);
+    end
+    disp('Next Tablet Center: ');
+    disp(nextAvailTablet);
+    
 end
 
-disp(d);
-if(~emptyStrip)
-    nextAvailTablet = d(1,:);
-end
-disp('Next Tablet Center: ');
-disp(nextAvailTablet);
-
-comPort = 'COM4';
-s = serial(comPort);
-set (s, 'DataBits' , 8 );
-set (s, 'StopBits' , 1 );
-set (s, 'BaudRate' , 9600 );
-set (s, 'Parity' , 'none' );
-fopen(s);
-fprintf(s, '%c', 'aa');
-fclose(s);
+%comPort = 'COM4';
+%s = serial(comPort);
+%set (s, 'DataBits' , 8 );
+%set (s, 'StopBits' , 1 );
+%set (s, 'BaudRate' , 9600 );
+%set (s, 'Parity' , 'none' );
+%fopen(s);
+%fprintf(s, '%c', 'aa');
+%fclose(s);
+disp('Finished');
 clear all;
 
---
+++ opencv-python/py_cir.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import operator
+
+img = cv2.imread('DSC_0196.jpg',0)
+img = cv2.medianBlur(img,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=30,maxRadius=40)
+
+circles = np.uint16(np.around(circles))
+print sorted(circles[0,:], key=lambda x : x[1])
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('Detected Medicines', cimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
